749eb5ca29b5ad090eb97bd82fe6f1deccbc323f
cjnissle <cjnissle@millersville.edu>
2021-02-14 14:54:48 -0500

    Update Save/Load
    - added Junit Load Test

--- src/test/java/team/indecision/SaveLoadTest.java
+++ src/test/java/team/indecision/SaveLoadTest.java
@@ -1,10 +1,16 @@
 package team.indecision;
 
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Paths;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
@@ -27,6 +33,46 @@ public class SaveLoadTest {
 		File classesTest = Paths.get("classesTest").toFile();
 		assertTrue(FileUtils.contentEquals(classes,classesTest));
 		// json is valid. It was checked using https://jsonlint.com.
+	}
+	
+	@Test
+	public void testLoad() {
+		
+		String name = "test";
+		Set<String> attributes = new HashSet<String>();
+		attributes.add("attr");
+		attributes.add("attr1");
+		SortedMap<String, String> relationships = new TreeMap<String, String>();
+		relationships.put("rel","type");
+		relationships.put("rel1","type");
+		Class c = new Class (name, attributes, relationships);
+		//System.out.println(c.toString());
+		
+		String name1 = "test1";
+		Class c1 = new Class (name1);
+		//System.out.println(c1.toString());
+		
+		String name2 = "test2";
+		Set<String> attributes2 = new HashSet<String>();
+		attributes2.add("attr");
+		SortedMap<String, String> relationships2 = new TreeMap<String, String>();
+		relationships2.put("rel","type");
+		Class c2 = new Class (name2, attributes2, relationships2);
+		//System.out.println(c2.toString());
+		
+		SortedMap<String, Class> classes = new TreeMap<String, Class>();
+		classes.put(name, c);
+		classes.put(name1, c1);
+		classes.put(name2, c2);
+		
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent)); //capturing console output.
+		UML.load("classesTest");
+		assertEquals("test [attr, attr1] {rel=type, rel1=type}\r\n" + 
+				"test1 [] {}\r\n" + 
+				"test2 [attr] {rel=type}", outContent.toString().trim());
+		System.setOut(System.out); // resetting the system.setOut to default
+		
 	}
 
 }
