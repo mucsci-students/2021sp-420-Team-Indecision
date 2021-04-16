ae882ebb72c3895f4305f33c3521adec27afbc77
cjnissle <70019683+cjnissle@users.noreply.github.com>
2021-04-16 14:04:16 -0400

    Updated Save/Load
    - created Test Cases for Save/Load Commands
    - Created 2 constructors for Save/Load commands one for GUI and one for CLI
    - Save and load for the cli now accepts absolute and relative paths.
    - GUI is now prepared to use native file chooser.

--- src/main/java/team/indecision/Command/LoadJSONCommand.java
+++ src/main/java/team/indecision/Command/LoadJSONCommand.java
@@ -1,5 +1,6 @@
 package team.indecision.Command;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.util.SortedMap;
 
@@ -20,14 +21,24 @@ public class LoadJSONCommand implements Command {
 	Classes model;
 	//Stores the file name of the file that will be loaded.
 	String fileName;
+	//Stores the file to be loaded this will be null if the cli controller is calling this command
+	File file;
 	//Stores whether or not the state of the model has changed. True if the state has changed false if not.
 	boolean stateChange;
 	
 	/** Constructs a Load JSON command with the desired model and file name.
 	 */
+	public LoadJSONCommand (Classes modelP, File fileP) {
+		model = modelP;
+		fileName = null;
+		file = fileP;
+		stateChange = false;
+	}
+	
 	public LoadJSONCommand (Classes modelP, String fileNameP) {
 		model = modelP;
 		fileName = fileNameP;
+		file = null;
 		stateChange = false;
 	}
 	
@@ -37,12 +48,17 @@ public class LoadJSONCommand implements Command {
 	@Override
 	public String execute() {
 		String response;
-		fileName = fileName.concat(".json");
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
-			SortedMap<String, Class> classes = objectMapper.readValue(Paths.get(fileName).toFile(), new TypeReference<SortedMap<String, Class>>() {});
-			model.setClasses(classes);
-			response = "Your data has been loaded from a JSON file in your program's root directory";
+			if (file == null) {
+				SortedMap<String, Class> classes = objectMapper.readValue(Paths.get(fileName).toFile(), new TypeReference<SortedMap<String, Class>>() {});
+				model.setClasses(classes);
+			}
+			else {
+				SortedMap<String, Class> classes = objectMapper.readValue(file, new TypeReference<SortedMap<String, Class>>() {});
+				model.setClasses(classes);
+			}
+			response = "Your data has been loaded from a JSON file.";
 			stateChange = true;
 		} catch (Exception ex) {
 			response = "Not a valid json file or the file does not exist.";
